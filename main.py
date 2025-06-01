789fe5c25e3320b89ea37d16d565084076072291
mjb44552 <m50booth@gmail.com>
2025-05-31 19:27:05 -0600

    changing git ignore file

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from source.hybrid_rag_agents import sql_input_agent, sql_output_agent
 from source.query_agents import query_sql_agents
 
-practice_queries = ['How many restruants and draggggg lifts does BreeeeckoooonRidge have?']
+practice_queries = ['What is the elevation of breckenridge?',
+                    'What is the average number of chairlifts at ski resorts in Canada?,'
+                    'What ski resort in Italy has the largest elevation range?',
+                    'How many ski resorts are in France and the United States?',]
 
 responses = query_sql_agents(queries=practice_queries,
                              input_agent=sql_input_agent,
